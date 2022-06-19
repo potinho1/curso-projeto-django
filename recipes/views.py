737e254229141b8afcf4036c008a72db27ab3019
joão Vitor Gomes Da Silva <gomesjoaovitor222@gmail.com>
2022-06-18 22:46:28 -0300

    Novo repositorio

---
+++ recipes/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render
+from utils.recipes.factory import make_recipe
+
+from .models import Recipe
+
+
+def home(request):
+    recipes = Recipe.objects.filter(
+        is_published=True
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
+
+
+def category(request, category_id):
+    recipes = Recipe.objects.filter(category__id=category_id,
+                                    is_published=True,).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipes(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
